28a3598c5baa9950a839e30c3d4d42cc899acf13
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-06-22 05:12:58 +0300

    Diploma backend version 1

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://mydiploma.students.nomoreparties.sbs',
     credentials: true,
   }),
 );
