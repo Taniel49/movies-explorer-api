241ebaf4d285fc8144c062bb8e83978f7bd29e8c
Mikhail Butyrskiy <laughingstorm49@gmail.com>
2022-10-03 12:14:17 +0300

    render deploy

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://movies-frontend-5qvt.onrender.com',
     credentials: true,
   }),
 );
